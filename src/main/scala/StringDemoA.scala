bb76a4c39d1f97d7934699b0242616030b243384
saidulu <saidulugolla984@gmail.com>
2026-01-31 09:10:59 +0530

    Stringclass

--- src/main/scala/StringDemoA.scala
+++ src/main/scala/StringDemoA.scala
@@ -11,6 +11,25 @@ object StringDemoA {
     // Method to get length of the string
     var lengthOfString = greetings.length()
     println("Length of the string greetings : greetings " + lengthOfString)
+
+    // concat method
+    var var1 = "Saidulu"
+    var var2 = "Golla"
+    var var0 = var1 + var2
+
+    println(var1 + var2)
+    println(var0.length)
+    var var3 = "Maithili"
+    var var4 = "Golla"
+
+    println(var3.concat((var4)))
+    println((var3 + var4).length)
+    println(var0.charAt(7))
+
+
+
+
+    //
   }
 
 }
