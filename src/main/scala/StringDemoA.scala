6c2812d90bb171e9974462f7f3adc2e65232cc3d
saidulu <saidulugolla984@gmail.com>
2026-01-31 10:08:13 +0530

    Stringclass

--- src/main/scala/StringDemoA.scala
+++ src/main/scala/StringDemoA.scala
@@ -29,7 +29,20 @@ object StringDemoA {
 
 
 
-    //
+    //Equals Method
+
+    var varA = "Hello Scala"
+    var varB = "Hello India"
+    println(varA.equals(varB)) // It gives true of both strings are having same  contents
+
+    println(varA == varB)     // Note : same as  equals .But  does  one additional step. It first check varA and varB are not Null.
+
+    // String Formatting
+    var nameOfCar = "Mercedes"
+    var costOfCar = 500000
+    var milageOfCar = 9.6
+    printf("Name of the car is %s and cost of the car is %d and milage of the car is %f", nameOfCar, costOfCar,milageOfCar)
+
   }
 
 }
