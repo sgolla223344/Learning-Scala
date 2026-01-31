add0ddf12d59996aa778f3458e2927f86e48a74d
saidulu <saidulugolla984@gmail.com>
2026-01-31 22:53:05 +0530

    PattrenMatch

---
+++ src/main/scala/PattrenMatch.scala
@@ -0,0 +1,12 @@
+object PattrenMatch {
+
+  def main(args: Array[String]): Unit = {
+    def matchPattern(x: Any) = x match {
+      case 1 => "One"
+      case 2 => "Two"
+      case 3 => "Three"
+        println("1")
+    }
+  }
+}
+
